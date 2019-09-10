91ba1a4bbabed0438a7d88229aa3eb1ef8537e77
MeiJamke <meijamke@gmail.com>
2019-09-10 17:53:29 +0800

    使用RecyclerView显示天气

---
+++ app/src/main/java/com/example/msunshine/ForecastAdapter.java
@@ -0,0 +1,51 @@
+package com.example.msunshine;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    ForecastAdapter() {
+
+    }
+
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        final TextView mWeatherTextView;
+
+        ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = view.findViewById(R.id.tv_weather_data);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.forecast_list_item, viewGroup, false);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ForecastAdapterViewHolder holder, int position) {
+        holder.mWeatherTextView.setText(mWeatherData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWeatherData != null)
+            return mWeatherData.length;
+        return 0;
+    }
+
+    void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/msunshine/MainActivity.java
+++ app/src/main/java/com/example/msunshine/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.msunshine;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,20 +20,28 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mWeatherDataText;
-    private EditText mSearchEditText;
+    private RecyclerView mRecyclerView;
+    private ForecastAdapter mForecastAdapter;
 
-    private TextView mErrorMsgShowTextView;
+    private EditText mSearchCity;
+    private TextView mErrorMsgDisplay;
     private ProgressBar mSearchProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWeatherDataText = findViewById(R.id.tv_weather_data);
-        mSearchEditText = findViewById(R.id.et_search);
+        mRecyclerView = findViewById(R.id.rv_forecast);
+        mForecastAdapter = new ForecastAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
-        mErrorMsgShowTextView = findViewById(R.id.tv_error_message);
+        mRecyclerView.setAdapter(mForecastAdapter);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mSearchCity = findViewById(R.id.et_search);
+        mErrorMsgDisplay = findViewById(R.id.tv_error_message);
         mSearchProgressBar = findViewById(R.id.pb_search_progress);
     }
 
@@ -43,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_search) {
+
+            mForecastAdapter.setWeatherData(null);
             loadWeatherData();
             return true;
         }
@@ -50,20 +63,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadWeatherData() {
-        String location = mSearchEditText.getText().toString();
+        String location = mSearchCity.getText().toString();
         new FetchFromUrl().execute(location);
     }
 
     public void showWeatherData() {
-        mWeatherDataText.setVisibility(View.VISIBLE);
-        mErrorMsgShowTextView.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mErrorMsgDisplay.setVisibility(View.INVISIBLE);
     }
 
     public void showErrorMessage() {
-        mWeatherDataText.setVisibility(View.INVISIBLE);
-        mErrorMsgShowTextView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMsgDisplay.setVisibility(View.VISIBLE);
     }
 
+    @SuppressLint("StaticFieldLeak")
     public class FetchFromUrl extends AsyncTask<String, Void, String[]> {
 
         @Override
@@ -95,12 +109,10 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String[] weatherData) {
 
             mSearchProgressBar.setVisibility(View.INVISIBLE);
-            mWeatherDataText.setText("");
 
             if (weatherData != null) {
                 showWeatherData();
-                for (String weather : weatherData)
-                    mWeatherDataText.append(weather);
+                mForecastAdapter.setWeatherData(weatherData);
             } else
                 showErrorMessage();
         }
