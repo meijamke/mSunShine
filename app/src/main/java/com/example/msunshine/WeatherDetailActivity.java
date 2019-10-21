362fdf44d720d814bfb9e8a708db37869b4c51b8
MeiJamke <meijamke@gmail.com>
2019-10-21 20:33:36 +0800

    add share function in detail layout

--- app/src/main/java/com/example/msunshine/WeatherDetailActivity.java
+++ app/src/main/java/com/example/msunshine/WeatherDetailActivity.java
@@ -3,11 +3,16 @@ package com.example.msunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.msunshine.data.ExplicitIntent;
 
 public class WeatherDetailActivity extends AppCompatActivity {
+
+    private String weatherData;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,7 +21,30 @@ public class WeatherDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent.hasExtra(ExplicitIntent.STRING_WEATHER_DATA)) {
-            String weatherData = intent.getStringExtra(ExplicitIntent.STRING_WEATHER_DATA);
+            weatherData = intent.getStringExtra(ExplicitIntent.STRING_WEATHER_DATA);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_share) {
+            shareWeather(weatherData);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareWeather(String weatherData) {
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle("choose which app to share")
+                .setType("text/*")
+                .setText(weatherData)
+                .startChooser();
     }
 }
